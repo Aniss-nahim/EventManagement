fb7accf9faa2c3fb85a7cf9700459a009c3b7ec4
Aniss Nahim <aniss202nahim@gmail.com>
2021-05-29 16:21:01 +0200

    hot fixe for event browsing

--- assets/components/Event.vue
+++ assets/components/Event.vue
@@ -8,7 +8,7 @@
                             <img class="rounded-circle border-success" style="height: 40px; width: 40px;" :src="userImage" alt="event creator" />
                         </div>
                         <div class="col-9">
-                            <p class="lead m-0 ml-2">  <a href="#" class="text-decoration-none text-success"><strong>{{ event.title }}</strong> </a> </p>
+                            <p class="lead m-0 ml-2">{{userName}}</p>
                             <small class="m-0 ml-2"><i class="fas fa-clock"></i> {{ createdAt }}</small><br/>
                         </div>
                         <div class="col-1">
@@ -22,7 +22,11 @@
                             </div>
                         </div>
                     </div>
-                    
+                    <div class="row">
+                        <div class="col-12">
+                            <p class="lead m-0 ml-2"> <i class="fas fa-bullhorn fa-xs"></i> <a href="#" class="text-decoration-none text-success"><strong>{{ event.title }}</strong> </a> </p>
+                        </div>
+                    </div>
                 </div>
                 <div class="card-body p-0">
                     <img class="img-fluid" :src="eventCover" alt="" />
@@ -30,6 +34,11 @@
                         <div class="row">
                             <div class="col-sm-12">
                                 <h6> <i class="fas fa-map-marker-alt"></i> {{event.address}}</h6>
+                            </div>
+                            <div class="col-12">
+                                <span v-for="(eventTag, index) in event.eventTags" :key="eventTag.id" class="badge badge-pill badge-success mr-2">
+                                    {{eventTag.tag.tagName}}
+                                </span>
                             </div>
                         </div>
                     </div>
@@ -78,6 +87,10 @@
 
             endDate : function(){
                 return moment(this.event.endDate).format('YYYY-MM-DD');
+            },
+
+            userName : function(){
+                return this.event.owner.lastName+' '+this.event.owner.firstName;
             },
 
             userImage : function(){
